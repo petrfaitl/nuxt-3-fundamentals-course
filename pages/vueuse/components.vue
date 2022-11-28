176c3d280f0defd9fb674e1fa8d2dd0c6719d18a
Petr Faitl <pfaitl@gmail.com>
2022-11-28 13:54:21 +1300

    Adds transition and components page

---
+++ pages/vueuse/components.vue
@@ -0,0 +1,44 @@
+<script lang="ts" setup>
+import {timestamp, useTimeAgo} from "@vueuse/core";
+
+const slider = ref(0);
+const value = computed(()=> timestamp() + slider.value **3);
+const timeAgo = useTimeAgo(value);
+
+
+</script>
+
+<template>
+  <div class="flex flex-col gap-6">
+    {{timeAgo}}
+    <input v-model="slider" class=" opacity-80 hover:opacity-100 bg-slate-200 h-2 appearance-none rounded-full slider" type="range" min="-3800" max="3800"/>
+    {{slider**3}}ms
+
+<!--    <div class="max-w-lg w-[500px] mx-auto p-8">-->
+<!--      <details  class="bg-white dark:bg-slate-900 ring-1 ring-black/5 dark:ring-white/10 shadow-lg p-6 rounded-lg">-->
+<!--        <summary class="text-sm leading-6 text-slate-900 dark:text-white font-semibold select-none">-->
+<!--          Why do they call it Ovaltine?-->
+<!--        </summary>-->
+<!--        <div class="mt-3 text-sm leading-6 text-slate-600 dark:text-slate-400">-->
+<!--          <p>The mug is round. The jar is round. They should call it Roundtine.</p>-->
+<!--        </div>-->
+<!--      </details>-->
+<!--    </div>-->
+  </div>
+</template>
+
+<style scoped>
+.slider{
+  /*-webkit-appearance: none;*/
+}
+
+.slider::-webkit-slider-thumb {
+  -webkit-appearance: none;
+  appearance: none;
+  width: 1.3rem;
+  height: 1.3rem;
+  cursor: pointer;
+  border-radius: 50%;
+  @apply bg-teal-600;
+}
+</style>
---
+++ pages/vueuse/transition.vue
@@ -0,0 +1,35 @@
+<script lang="ts" setup>
+
+import {TransitionPresets, useTransition} from "@vueuse/core";
+
+
+const width = ref(0.1);
+const animatedWidth = useTransition(width, {transition:TransitionPresets.easeInCubic, duration:3000});
+
+const load = _ => {
+  width.value = 100;
+}
+
+const reset = _ => {
+  width.value = 0.1;
+}
+</script>
+
+<template>
+  <div>
+    <div class="flex gap-2">
+      <div class="bg-teal-500 h-4 bar"></div>
+      <div class="ml-auto">{{Math.floor(animatedWidth)}}</div>
+    </div>
+    <div class="flex gap-4">
+      <button @click="load" class="btn-submit">Load</button>
+      <button @click="reset" class="btn-submit">Reset</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.bar {
+  width: v-bind(animatedWidth+ "%");
+}
+</style>
